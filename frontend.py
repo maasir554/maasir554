29e415316cca1529246cd1f2c6514d8f1d37094a
maasir554 <96122569+maasir554@users.noreply.github.com>
2022-02-14 02:41:37 +0530

    added category to table items, mistake in logic
    corrected silly mistakes in logic

--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 #importing the connector :-
-from distutils.util import execute
+#from distutils.util import execute
 import mysql.connector as c
 #defining the database :-
 #Note: the name of database is Eshop. you have to create database Eshop to run this program.
@@ -7,7 +7,7 @@ database = c.connect(host = "localhost", user = 'root', password = "password", d
 #defining cursor(it will be used to excute SQL commands) :-
 cursor = database.cursor(buffered=True)
 print('You are connected to the server eshop successfully.')
-cursor.execute('CREATE TABLE IF NOT EXISTS items(item_id INT(4) PRIMARY KEY NOT NULL,item_name VARCHAR(50),seller_id INT(4) NOT NULL,price INT(8) NOT NULL,date_added DATE,rating FLOAT(2,1))' )
+cursor.execute('CREATE TABLE IF NOT EXISTS items(item_id INT(4) PRIMARY KEY NOT NULL,item_name VARCHAR(50),category VARCHAR(30),seller_id INT(4) NOT NULL,price INT(8) NOT NULL,date_added DATE,rating FLOAT(2,1))' )
 cursor.execute('CREATE TABLE IF NOT EXISTS customers(customer_id INT(4) PRIMARY KEY NOT NULL,customer_name VARCHAR(30) NOT NULL,customer_age INT(3),gender ENUM("male","female","others"),email VARCHAR(30),mobile VARCHAR(12),city VARCHAR(20))')
 cursor.execute('CREATE TABLE IF NOT EXISTS delboy(delboy_id INT(4) PRIMARY KEY NOT NULL,name VARCHAR(30) NOT NULL,city VARCHAR(20),mobile VARCHAR(12),salary INT(4),rating float(2,1))')
 cursor.execute('CREATE TABLE IF NOT EXISTS sellers(seller_id INT(4) PRIMARY KEY NOT NULL,name VARCHAR(30) NOT NULL,city VARCHAR(20),mobile VARCHAR(12),email VARCHAR(30),rating float(2,1))')
@@ -54,7 +54,7 @@ def menu():
             menu()
     
     elif(a==3):
-        print('Thank You! for using this program, Have a nice day! \n you can now exit or call functions manually. \nlistof the functions(prefix add_ OR delete_ before them) : customer\nseller\nitem\norder\ndelboy\ncomplain')
+        print('Thank You! for using this program, Have a nice day! \n you can now exit or call functions manually. \nlistof the functions(prefix add_ OR delete_ before them) : customer\nseller\nitem\norder\ndelboy\ncomplain\n--- menu closed ---')
     else:
         print('please enter a valid response')
         menu()
@@ -98,7 +98,7 @@ def add_seller():
     print('data inserted to table sellers successfully!')
     b = input("do you want ot add more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        add_seller()
     elif (b=='n'):
         menu()
     else:
@@ -108,18 +108,19 @@ def add_item():
     print("add_item() function started.")
     item_id = input('enter item ID  : ')
     name = input('enter item name : ')
+    cat = input('enter category of item : ')
     sid = input('enter seller_id of item seller : ')
     price = input('enter price of item : ')
     date_added = input('enter date added : ')
     rating = input('enter rating of item : ')
-    data = (item_id,name,sid,price,date_added,rating)
-    statement = "INSERT INTO items VALUES(%s,%s,%s,%s,%s,%s)"
+    data = (item_id,name,cat,sid,price,date_added,rating)
+    statement = "INSERT INTO items VALUES(%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(statement,data)
     database.commit()
     print('data inserted to table items successfully!')
     b = input("do you want ot add more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        add_item()
     elif (b=='n'):
         menu()
     else:
@@ -139,7 +140,7 @@ def add_order():
     print('data inserted to the table ORDERS successfully!')
     b = input("do you want ot add more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        add_order()
     elif (b=='n'):
         menu()
     else:
@@ -160,7 +161,7 @@ def add_delboy():
     print('data inserted to table delboy successfully!')
     b = input("do you want ot add more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        add_delboy()
     elif (b=='n'):
         menu()
     else:
@@ -178,7 +179,7 @@ def add_complain():
     print("data inserted to the table COMPLAINS successfully!")
     b = input("do you want ot add more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        add_complain()
     elif (b=='n'):
         menu()
     else:
@@ -193,7 +194,7 @@ def delete_customer():
     print('deleted the customer data of customer_id : ',cid,'from table customers successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_customer()
     elif (b=='n'):
         menu()
     else:
@@ -207,7 +208,7 @@ def delete_seller():
     print('deleted the seller data of seller_id : ',sid,' from table sellers successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_seller()
     elif (b=='n'):
         menu()
     else:
@@ -221,7 +222,7 @@ def delete_item():
     print('deleted the item data of item_id : ',iid,' from table items successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_item()
     elif (b=='n'):
         menu()
     else:
@@ -235,7 +236,7 @@ def delete_order():
     print('deleted the item data of order_id : ',x,' from table orders successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_order()
     elif (b=='n'):
         menu()
     else:
@@ -249,7 +250,7 @@ def delete_delboy():
     print('deleted the data of delboy_id ',x,' from table delboy successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_delboy()
     elif (b=='n'):
         menu()
     else:
@@ -263,7 +264,7 @@ def delete_complain():
     print('deleted the data of complain_id ',x,' from table complains successfully!')
     b = input("do you want ot delete more data to THIS table? (y/n) : ")
     if (b=='y'):
-        add_customer()
+        delete_complain()
     elif (b=='n'):
         menu()
     else:
